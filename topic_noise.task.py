2e56fd5d9946a8d21e2cd2263dee76d1f2465eff
Stephan Gabler <stephan.gabler@gmail.com>
2011-05-13 16:02:38 +0200

    create plots for all queries

--- topic_noise.task.py
+++ topic_noise.task.py
@@ -7,45 +7,70 @@ correlate wikipedia article similarity with human perceived similarity of terms
 Created by Stephan Gabler on 2011-05-12.
 Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 """
-import matplotlib
-matplotlib.use("Agg")
 
 import sys
 import os
+from os import path
+import logging
 
 import string
 import pickle
-from gensim import models
-from gensim import matutils
-from gensim.corpora import Dictionary
+
+# import matplotlib
+# matplotlib.use("Agg")
 import numpy as np
 import pylab as plt
 import scipy.stats
+from sumatra.parameters import build_parameters
+from gensim import models, matutils
+from gensim.corpora import Dictionary
 
 
 def order(l, indices):
     """reorder a list according to the indices"""
     return [l[i] for i in indices]
 
-base = '/Users/dedan/projects/mpi/data/'
+
+# read the parameters and create output folder
+parameter_file = sys.argv[1]
+p = build_parameters(parameter_file)
+result_path = path.join(p['base_path'], p['result_path'])
+output_dir = path.join(result_path, 'test1') #p['sumatra_label'])
+if not path.exists(output_dir):
+    os.mkdir(output_dir)
+
+# set up the logger to print to a file and stdout
+logger = logging.getLogger('gensim')
+file_handler = logging.FileHandler(path.join(output_dir, "run.log"), 'w')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+logger.setLevel(logging.DEBUG)
+logger.info("running %s" % ' '.join(sys.argv))
+
 
 # read in the id to word mapping
 id_word = {}
-with open(base + 'corpora/sparql/id_word.txt') as f:
+with open(os.path.join(p['base_path'], p['sparql_path'], 'id_word.txt')) as f:
     for line in f.readlines():
-        arr = line.split()
-        idx = arr[0]
-        word = " ".join(arr[1:])
+        idx, word = line.strip().split('\t')
         id_word[idx] = word
 
 # read in model and dictionary
-dictionary = Dictionary.loadFromText(base + "corpora/wiki/wiki-mar2008/head500.noblanks.cor_wordids.txt")
-lsi = pickle.load(open(base + "results/20110429-143751/lsi.model"))
-pre = pickle.load(open(base + "results/20110429-143751/pre.model"))
+logger.info('loading models')
+dictionary = Dictionary.loadFromText(os.path.join(p['base_path'],
+                                                  p['dict_path']))
+model_path = os.path.join(result_path, p['model_label'])
+lsi = pickle.load(open(os.path.join(model_path, 'lsi.model')))
+pre = pickle.load(open(os.path.join(model_path, 'pre.model')))
 
 # and get all the wikipedia articles
-wiki = pickle.load(open(base + "results/test/articles.pickle"))
-info = pickle.load(open(base + "results/test/info.pickle"))
+article_path = os.path.join(result_path, p['article_label'])
+wiki = pickle.load(open(os.path.join(article_path, 'articles.pickle')))
+info = pickle.load(open(os.path.join(article_path, 'info.pickle')))
 
 not_found = []
 
@@ -60,51 +85,56 @@ with open('/Users/dedan/projects/mpi/data/corpora/sparql/reference_queries.txt')
         except KeyError:
             not_found.append(term)
 
-print len(not_found)
-print wiki['hamster']['Adolescent_Radioactive_Black_Belt_Hamsters']['rating']
+logger.info("%d words from the reference queries not found" % len(not_found))
 
+for query_key in wiki.keys():
+    logger.info("working on: %s" % query_key)
+    query = wiki[query_key]
+    n = len(query)
+    sim_res = np.zeros((n,n))
 
-hamster = wiki['hamster']
-n = len(hamster)
-res = np.zeros((n,n))
+    for i, val1 in enumerate(query.itervalues()):
+        for j, val2 in enumerate(query.itervalues()):
+    
+            bow1 = dictionary.doc2bow(val1['text'])
+            bow2 = dictionary.doc2bow(val2['text'])
+            sim_res[i,j] = matutils.cossim(lsi[pre[bow1]], lsi[pre[bow2]])
+    avg = np.mean(sim_res, axis=0)
+    idx = np.argsort(avg)
 
-for i, val1 in enumerate(hamster.itervalues()):
-    for j, val2 in enumerate(hamster.itervalues()):
-        
-        bow1 = dictionary.doc2bow(val1['text'])
-        bow2 = dictionary.doc2bow(val2['text'])
-        res[i,j] = matutils.cossim(lsi[pre[bow1]], lsi[pre[bow2]])
-avg = np.mean(res, axis=0)
-idx = np.argsort(avg)
+    human = []
+    for key in query.keys():
+        if query[key].has_key('rating'):
+            human.append(query[key]['rating'])
+        else:
+            logger.warning("%s has no rating" % key)
+    # human = [val['rating'] for val in query.itervalues()]
 
+    # compute correlation with human rating
+    res = np.zeros((n,2))
+    for i in range(n):
+        human_r = [human[j] for j in sorted(idx[i:])]
+        avg_r = [avg[j] for j in sorted(idx[i:])]
+        r, p = scipy.stats.pearsonr(human_r, avg_r)
+        res[i,0] = r
+        res[i,1] = p
 
-human = [hamster[key]['rating'] for key in hamster.keys()]
+    fig = plt.figure()
+    
+    # plot correlation
+    ax = fig.add_subplot(2,1,1)
+    ax.plot(res)
+    ax.legend(['r', 'p'])
 
-cor = scipy.stats.pearsonr(human, avg)
-print(cor)
-cor = scipy.stats.pearsonr(sorted(human), sorted(avg))
-print(cor)
+    # plot similarity distribution
+    ax = fig.add_subplot(2,1,2)
+    ax.bar(range(n), avg[idx])
 
-res = np.zeros((n,2))
-for i in range(n):
-    human_r = [human[j] for j in sorted(idx[i:])]
-    avg_r = [avg[j] for j in sorted(idx[i:])]
-    r, p = scipy.stats.pearsonr(human_r, avg_r)
-    res[i,0] = r
-    res[i,1] = p
+    # Set the x tick labels to the group_labels defined above and rotate labels
+    ax.set_xticks(range(n))
+    k = [key + ' ' + str(query[key]['rating']) for key in query.keys()]
+    ax.set_xticklabels(order(k, idx)) 
+    fig.autofmt_xdate()
 
-fig = plt.figure()
-ax = fig.add_subplot(2,1,1)
-ax.plot(res)
-
-ax = fig.add_subplot(2,1,2)
-ax.bar(range(n), avg[idx])
-
-# Set the x tick labels to the group_labels defined above and rotate labels
-ax.set_xticks(range(n))
-k = [key + str(hamster[key]['rating']) for key in hamster.keys()]
-ax.set_xticklabels(order(k, idx)) 
-fig.autofmt_xdate()
-
-plt.savefig(os.path.join(base, 'results/test/hamster.pdf'))
-plt.close()
+    plt.savefig(os.path.join(output_dir, query_key + '.pdf'))
+    plt.close()
