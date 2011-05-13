dac1e41f42452ee0941d44c7295be5b3d9b189d8
Stephan Gabler <stephan.gabler@gmail.com>
2011-05-13 14:19:14 +0200

    first running prototype of the topic noise analysis

--- topic_noise.task.py
+++ topic_noise.task.py
@@ -7,6 +7,8 @@ correlate wikipedia article similarity with human perceived similarity of terms
 Created by Stephan Gabler on 2011-05-12.
 Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 """
+import matplotlib
+matplotlib.use("Agg")
 
 import sys
 import os
@@ -18,44 +20,51 @@ from gensim import matutils
 from gensim.corpora import Dictionary
 import numpy as np
 import pylab as plt
+import scipy.stats
+
 
 def order(l, indices):
     """reorder a list according to the indices"""
     return [l[i] for i in indices]
 
+base = '/Users/dedan/projects/mpi/data/'
 
-
-# the format is  queryID- Global ID - Local ID  title  rating confidence
-
+# read in the id to word mapping
 id_word = {}
-with open('/Users/dedan/projects/mpi/data/corpora/sparql/id_word.txt') as f:
+with open(base + 'corpora/sparql/id_word.txt') as f:
     for line in f.readlines():
         arr = line.split()
         idx = arr[0]
         word = " ".join(arr[1:])
         id_word[idx] = word
 
+# read in model and dictionary
+dictionary = Dictionary.loadFromText(base + "corpora/wiki/wiki-mar2008/head500.noblanks.cor_wordids.txt")
+lsi = pickle.load(open(base + "results/20110429-143751/lsi.model"))
+pre = pickle.load(open(base + "results/20110429-143751/pre.model"))
 
-dictionary = Dictionary.loadFromText("/Users/dedan/projects/mpi/data/corpora/wiki/wiki-mar2008/head500.noblanks.cor_wordids.txt")
-lsi = pickle.load(open("/Users/dedan/projects/mpi/data/results/20110429-143751/lsi.model"))
-pre = pickle.load(open("/Users/dedan/projects/mpi/data/results/20110429-143751/pre.model"))
+# and get all the wikipedia articles
+wiki = pickle.load(open(base + "results/test/articles.pickle"))
+info = pickle.load(open(base + "results/test/info.pickle"))
 
-wiki = pickle.load(open("/Users/dedan/projects/mpi/data/results/sparql_wiki.pickle"))
-print wiki['kangaroos'].keys()
+not_found = []
+
+#add human rating to the wikipedia data
 with open('/Users/dedan/projects/mpi/data/corpora/sparql/reference_queries.txt') as f:
     for line in f.readlines():
         arr = line.split()
         word = id_word[arr[0]]
-        term = string.replace(arr[3], "_", " ")
-        wiki[word][term]['rating'] = int(arr[4])
+        term = arr[3]
+        try:
+            wiki[word][term]['rating'] = int(arr[4])
+        except KeyError:
+            not_found.append(term)
 
+print len(not_found)
+print wiki['hamster']['Adolescent_Radioactive_Black_Belt_Hamsters']['rating']
 
-print wiki.keys()
-print wiki['hamster']['Adolescent Radioactive Black Belt Hamsters']['rating']
-crash
 
 hamster = wiki['hamster']
-keys = []
 n = len(hamster)
 res = np.zeros((n,n))
 
@@ -65,17 +74,37 @@ for i, val1 in enumerate(hamster.itervalues()):
         bow1 = dictionary.doc2bow(val1['text'])
         bow2 = dictionary.doc2bow(val2['text'])
         res[i,j] = matutils.cossim(lsi[pre[bow1]], lsi[pre[bow2]])
-    
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-
 avg = np.mean(res, axis=0)
 idx = np.argsort(avg)
-ax.bar(np.arange(n), avg[idx])
+
+
+human = [hamster[key]['rating'] for key in hamster.keys()]
+
+cor = scipy.stats.pearsonr(human, avg)
+print(cor)
+cor = scipy.stats.pearsonr(sorted(human), sorted(avg))
+print(cor)
+
+res = np.zeros((n,2))
+for i in range(n):
+    human_r = [human[j] for j in sorted(idx[i:])]
+    avg_r = [avg[j] for j in sorted(idx[i:])]
+    r, p = scipy.stats.pearsonr(human_r, avg_r)
+    res[i,0] = r
+    res[i,1] = p
+
+fig = plt.figure()
+ax = fig.add_subplot(2,1,1)
+ax.plot(res)
+
+ax = fig.add_subplot(2,1,2)
+ax.bar(range(n), avg[idx])
 
 # Set the x tick labels to the group_labels defined above and rotate labels
-ax.set_xticks(x)
-ax.set_xticklabels(order(hamster.keys(), idx)) 
+ax.set_xticks(range(n))
+k = [key + str(hamster[key]['rating']) for key in hamster.keys()]
+ax.set_xticklabels(order(k, idx)) 
 fig.autofmt_xdate()
 
-plt.show()
+plt.savefig(os.path.join(base, 'results/test/hamster.pdf'))
+plt.close()
