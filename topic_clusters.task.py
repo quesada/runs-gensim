af96076c2758a228539b541291468c4659cd20a6
Stephan Gabler <stephan.gabler@gmail.com>
2011-06-03 17:32:32 +0200

    separated cluster analyses in a task and a view

--- topic_clusters.task.py
+++ topic_clusters.task.py
@@ -6,8 +6,8 @@ clusters.py
 with get_wiki_articles I download the wikipedia articles for all results of a
 sparql query. These articles are then transformed to an lsi space and
 projected on the first 2 principal components of this space in order to look
-for clusters. The points in the plots are colored (grayscaled) by the value
-a human gave as rating of how good this term fits the sparqle query termn.
+for clusters. This script only saves the cluster data to a file which can
+then be visualized with the corresponding viewer.
 """
 
 import sys
@@ -15,10 +15,7 @@ import os
 from os import path
 import pickle
 
-import matplotlib
-matplotlib.use("Agg")
 import numpy as np
-import pylab as plt
 from gensim.corpora import Dictionary
 from sumatra.parameters import build_parameters
 import tools
@@ -45,27 +42,23 @@ if int(p['num_topics']) > lsi.numTopics:
     logger.error('model to small')
 lsi.numTopics = int(p['num_topics'])
 
-for topic, data in wiki.iteritems():
+data = {}
+for topic, entries in wiki.iteritems():
     logger.info('working on: %s' % topic)
 
-    keys = []
-    vecs = []
-    ratings = []
-    for key, val in data.iteritems():
-        keys.append(key)
-        vecs.append(lsi[pre[dictionary.doc2bow(val['text'])]])
-        ratings.append(val['rating'])
-    vecs = np.squeeze(np.array(vecs)[:,:, 1:2]).T
+    data[topic] = {}
+    data[topic]['keys'] = []
+    data[topic]['vecs'] = []
+    data[topic]['ratings'] = []
+    for key, val in entries.iteritems():
+        data[topic]['keys'].append(key)
+        data[topic]['vecs'].append(lsi[pre[dictionary.doc2bow(val['text'])]])
+        data[topic]['ratings'].append(val['rating'])
+    data[topic]['vecs'] = np.squeeze(np.array(data[topic]['vecs'])[:,:, 1:2]).T
 
-    U, d, V = np.linalg.svd(vecs, full_matrices=False)
-    proj = np.dot(U[:,0:2].T, vecs)
+    U, d, V = np.linalg.svd(data[topic]['vecs'], full_matrices=False)
+    data[topic]['U'] = U
+    data[topic]['d'] = d
 
-    plt.figure()
-    plt.subplot(2,1,1)
-    for i in range(proj.shape[1]):
-        col = (1- (ratings[i] / 100.0)) * 0.7
-        plt.plot(proj[0, i], proj[1, i], '.', color=('%f' % col))
-    plt.subplot(2,1,2)
-    plt.plot(d)
-    plt.savefig(path.join(output_dir, topic + '.' + p['format']))
-    plt.close()
+f = open(os.path.join(output_dir, "data.pickle"), 'wb')
+pickle.dump(data, f)
