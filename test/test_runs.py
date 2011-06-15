5710ab4929ef2f17d3f7d00e08ec829090399fcd
Stephan Gabler <stephan.gabler@gmail.com>
2011-06-15 10:14:05 +0200

    create a test module

---
+++ test/test_runs.py
@@ -0,0 +1,17 @@
+'''
+Created on 15.06.2011
+
+@author: dedan
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def test_get_wiki_articles_run(self):
+        
+
+
+if __name__ == "__main__":
+    unittest.main()
