3fb4aaaccb0c68509971eb499bd73d06f31dd1ae
Stephan Gabler <stephan.gabler@gmail.com>
2011-06-15 15:37:46 +0200

    prepare get_wiki_articles for testing
    add a main method that can be called in the test
    
    removed a bug: the terms from redirections where not properly unquoted when I tried to retrieve the article. This often led to empty text for an article

--- get_wiki_articles_run.py
+++ get_wiki_articles_run.py
@@ -7,126 +7,140 @@ Created by Stephan Gabler on 2011-05-12.
 Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 """
 
-import sys
-import os
-import mwclient
-import glob
-import codecs
-import pickle
-import unicodedata as ud
-import logging
-import re
-import tools
-
-from sumatra.parameters import build_parameters
 from gensim.corpora import wikicorpus
 from gensim.parsing.preprocessing import preprocess_string
+from sumatra.parameters import build_parameters
+import codecs
+import glob
+import mwclient
+import os
+import pickle
+import re
+import sys
+import tools
+import unicodedata as ud
+import urllib
 
-# setup
-p = build_parameters(sys.argv[1])
-output_dir = os.path.join(p['base_path'], p['result_path'], p['sumatra_label'])
-if not os.path.exists(output_dir):
-    os.mkdir(output_dir)
-logger = tools.get_logger('gensim', os.path.join(output_dir, "run.log"))
-logger.info("running %s" % ' '.join(sys.argv))
 
-# initializations
-articles = {}
-all_missing = []
-redir_on = {}
-collisions = {}
-site = mwclient.Site('en.wikipedia.org', '/w/api.php/')
+def main(param_file=None):
 
-# get all txt files in a folder and iterate over them
-filelist = glob.glob(os.path.join(p['base_path'],
-                                  p['folder_path'],
-                                  "*.txt"))
-for f in filelist:
+    # setup
+    if param_file:
+        p = build_parameters(param_file)
+    else:
+        p = build_parameters(sys.argv[1])
+    output_dir = os.path.join(p['base_path'],
+                              p['result_path'],
+                              p['sumatra_label'])
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    logger = tools.get_logger('gensim', os.path.join(output_dir, "run.log"))
+    logger.info("running %s" % ' '.join(sys.argv))
 
-    # get the word we are working on
-    f_name = os.path.basename(f)
-    k_word = os.path.splitext(f_name)[0]
-    logger.info("working on file: %s" % f_name)
+    # initializations
+    articles = {}
+    all_missing = []
+    redir_on = {}
+    collisions = {}
+    non_ascii = []
+    site = mwclient.Site('en.wikipedia.org', '/w/api.php/')
 
-    # try to convert the word into ascii for the http query
-    file_obj = codecs.open(f, "r", "utf-16")
-    counter = 0
-    words = []
-    for w in file_obj.readlines():
-        try:
-            s = w.strip().decode('ascii')
-            words.append(s)
-        except Exception:
-            counter += 1
-    logger.info("\t%d words with non ascii characters are ommited" % counter)
+    # get all txt files in a folder and iterate over them
+    filelist = glob.glob(os.path.join(p['base_path'],
+                                      p['folder_path'],
+                                      "*.txt"))
+    for f in filelist:
 
-    articles[k_word] = {}
-    logger.info("\tfound %d words in file" % len(words))
+        # get the word we are working on
+        f_name = os.path.basename(f)
+        k_word = os.path.splitext(f_name)[0]
+        logger.info("working on file: %s" % f_name)
 
-    for word in words:
-        data = {}
-        page = site.Pages[word]
+        # try to convert the word into ascii for the http query
+        file_obj = codecs.open(f, "r", "utf-16")
+        counter = 0
+        words = []
+        for w in file_obj.readlines():
+            try:
+                s = w.strip().decode('ascii')
+                words.append(s)
+            except Exception:
+                counter += 1
+                non_ascii.append(w.strip())
+        logger.info("\t%d words containing non ascii are ommited" % counter)
 
-        # follow the redirect and check for collisions
-        if page.redirect:
-            res = re.search('\[\[(.+)\]\]', page.edit())
-            redir_word = res.groups()[0]
-            if redir_word in redir_on:
-                logger.warning("[%s AND %s] both redirect on --> %s" %
+        articles[k_word] = {}
+        logger.info("\tfound %d words in file" % len(words))
+
+        for word in words:
+            data = {}
+            page = site.Pages[word]
+
+            # follow the redirect and check for collisions
+            if page.redirect:
+                res = re.search('\[\[(.+)\]\]', page.edit())
+                redir_word = urllib.unquote(res.groups()[0])
+                if redir_word in redir_on:
+                    logger.warning("[%s AND %s] both redirect on --> %s" %
                                     (word, redir_on[redir_word], redir_word))
-                collisions[redir_word] = redir_on[redir_word]
+                    collisions[redir_word] = redir_on[redir_word]
+                else:
+                    logger.info("[%s] redir from [%s]" % (redir_word, word))
+                    redir_on[redir_word] = word
+                text = site.Pages[redir_word].edit()
+                data['redirected'] = redir_word
+
             else:
-                logger.info("[%s] redir from [%s]" % (redir_word, word))
-                redir_on[redir_word] = word
-            text = site.Pages[redir_word].edit()
-            data['redirected'] = redir_word
+                text = page.edit()
 
-        else:
-            text = page.edit()
+            # check for missing wikipedia articles
+            if  text == "":
+                all_missing.append(word)
+                continue
 
-        # check for missing wikipedia articles
-        if  text == "":
-            all_missing.append(word)
-            continue
+            # preprocess the received article
+            data['text'] = wikicorpus.filter_wiki(text)
+            in_ascii = ud.normalize('NFKD',
+                                    data['text']).encode('ascii', 'ignore')
+            data['text'] = preprocess_string(in_ascii)
+            articles[k_word][word] = data
 
-        # preprocess the received article
-        data['text'] = wikicorpus.filterWiki(text)
-        in_ascii = ud.normalize('NFKD',
-                                data['text']).encode('ascii', 'ignore')
-        data['text'] = preprocess_string(in_ascii)
-        articles[k_word][word] = data
+    logger.info('add human rating to the articles')
+    id_word = {}
+    sparql_path = os.path.join(p['base_path'], p['sparql_path'])
+    with open(os.path.join(sparql_path, 'id_word.txt')) as f:
+        for line in f.readlines():
+            idx, word = line.strip().split('\t')
+            id_word[idx] = word
 
-logger.info('add human rating to the articles')
-id_word = {}
-with open(os.path.join(p['base_path'], p['sparql_path'], 'id_word.txt')) as f:
-    for line in f.readlines():
-        idx, word = line.strip().split('\t')
-        id_word[idx] = word
+    #add human rating to the wikipedia data
+    not_found = []
+    with open(os.path.join(sparql_path, p['human_file'])) as f:
+        for line in f.readlines():
+            arr = line.split()
+            word = id_word[arr[0]]
+            term = arr[3]
+            try:
+                articles[word][term]['rating'] = int(arr[4])
+            except KeyError:
+                not_found.append(term)
+    logger.info("%d words from the ref queries not found" % len(not_found))
 
-#add human rating to the wikipedia data
-not_found = []
-with open(os.path.join(p['base_path'], p['sparql_path'], p['human_file'])) as f:
-    for line in f.readlines():
-        arr = line.split()
-        word = id_word[arr[0]]
-        term = arr[3]
-        try:
-            articles[word][term]['rating'] = int(arr[4])
-        except KeyError:
-            not_found.append(term)
-logger.info("%d words from the reference queries not found" % len(not_found))
+    f = open(os.path.join(output_dir, "articles.pickle"), 'wb')
+    pickle.dump(articles, f)
+    f.close
 
-f = open(os.path.join(output_dir, "articles.pickle"), 'wb')
-pickle.dump(articles, f)
-f.close
+    info = {}
+    info['missing'] = all_missing
+    info['redirs'] = redir_on
+    info['collisions'] = collisions
+    info['not_found'] = not_found
+    info['non_ascii'] = non_ascii
+    f = open(os.path.join(output_dir, "info.pickle"), 'wb')
+    pickle.dump(info, f)
+    f.close
 
-info = {}
-info['missing'] = all_missing
-info['redirs'] = redir_on
-info['collisions'] = collisions
-info['not_found'] = not_found
-f = open(os.path.join(output_dir, "info.pickle"), 'wb')
-pickle.dump(info, f)
-f.close
+    logger.info("%d redirecting collisions (see info.pickle)" % len(collisions))
 
-logger.info("%d redirecting collisions (see info.pickle)" % len(collisions))
+if __name__ == '__main__':
+    main()
